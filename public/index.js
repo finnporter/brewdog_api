644c4d2fe28fecf61ced2b6f7d6569e0f3083fd5
finnporter <iamfinnporter@gmail.com>
2017-08-02 20:35:36 +0100

    displays a list of beers and their pictures

--- public/index.js
+++ public/index.js
@@ -9,10 +9,34 @@ var makeRequest = function(url, callback) {
 
 var requestComplete = function() {
   console.log("this. is:", this);
+
   if (this.status !== 200) return;
   var jsonString = this.responseText;
   var beers = JSON.parse(jsonString);
+
+  populateBeerList(beers);
+  console.log(beers)
+};
+
+var populateBeerList = function(beers) {
+
+  beers.forEach(function(beer) {
+    displayBeer(beer);
+  });
+};
+
+var displayBeer = function(beer) {
+  var ul = document.querySelector('#beer-list');
+  var liName = document.createElement('li');
+  var liImg = document.createElement('img');
+  liName.innerText = beer.name;
+  liImg.src = beer.image_url;
+  liImg.height = 150;
+  ul.appendChild(liName);
+  ul.appendChild(liImg);
 }
+
+
 
 var app = function () {
   var url = "https://api.punkapi.com/v2/beers";
